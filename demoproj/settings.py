84e2442b84447889fc9b74ee8cca596d8df0ec22
Anant Pednekar <x21188947@student.ncirl.ie>
2023-02-10 17:47:51 +0000

    added changes till moes application

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '-1w@rvnkatx(!qiw8)$97w)lg6j8+5rwwe=vbrf-ll%dpaizn&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["5b8fdb2c6bde4b76bf000a28a5562c46.vfs.cloud9.eu-west-1.amazonaws.com"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
