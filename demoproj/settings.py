9867da864fb07467c4d1bb719d8da4a26fbfc41e
Anant Pednekar <x21188947@student.ncirl.ie>
2023-02-14 14:06:31 +0000

    added changes till ValidateUserInput

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["5b8fdb2c6bde4b76bf000a28a5562c46.vfs.cloud9.eu-west-1.amazonaw
 
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
